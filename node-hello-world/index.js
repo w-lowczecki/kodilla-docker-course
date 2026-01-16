cf07242c8769f648ff08954c5c2cf0094a414176
Wojciech Lowczecki <w.lowczecki@proton.me>
2026-01-16 15:07:16 +0100

    node-hello-world:1.1.0

--- node-hello-world/index.js
+++ node-hello-world/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/hello", (req, res) => {
-    res.send("Hello World from NOde Application");
+    res.send("Hello from Node Application!");
 });
 
 app.listen(port, () => {
